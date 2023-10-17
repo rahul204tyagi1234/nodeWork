477f48e3e8eb87307119961a86653d5515d4b803
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-10-17 20:31:23 +0530

    today work added

---
+++ 7_create_export/importExport/index.js
@@ -0,0 +1,21 @@
+const oper =require ("./oper");
+console.log(oper);
+ console.log(oper.add(15,5));
+ console.log(oper.sub(15,5));
+
+const {add,sub,multiply, name} =require ("./oper");
+
+ console.log(add(15,5));
+ console.log(sub(15,5));
+ console.log(multiply(15,5));
+ console.log(name);
+
+
+const add =require ("./oper");
+console.log(add(15,5));
+
+const sub =require ("./oper");
+console.log(sub(15,5));
+
+const name =require ("./oper");
+console.log(name);
