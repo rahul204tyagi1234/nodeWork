2bffc4e1497ebb24d149ad526f2b87acb36c1090
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-11-03 21:01:30 +0530

    add api

--- 20_api_with_database/restfulapi/src/app.js
+++ 20_api_with_database/restfulapi/src/app.js
@@ -1,3 +1,22 @@
+// const express =require("express");
+// require("./db/connectio")
+// // const student = require("./models/student")
+// const app =express();
+// const port = process.env.PORT || 3000; 
+
+// app.get("/",(req,res)=>{
+//     res.send("hello");
+// })
+
+// // create a new student
+// app.post("/student",(req,res)=>{
+//     res.send("hello from the student side .");
+// })
+// app.listen(port,()=>{
+//     console.log(`connection is setup at ${port}`);
+// })
+
+
 const express =require("express");
 require("./db/connectio")
 const app =express();
--- 20_api_with_database/restfulapi/src/db/connectio.js
+++ 20_api_with_database/restfulapi/src/db/connectio.js
@@ -1,10 +1,24 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/students-api",{
-    useCreateIndex:true,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    console.log("connection is successfull");
-}).catch((err)=>{
-    console.log("no connection");
-})
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/students-api",{
+//     useCreateIndex:true,
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// }).then(()=>{
+//     console.log("connection is successfull");
+// }).catch((err)=>{
+//     console.log("no connection");
+// })
+
+// const {MongoClient}=require("mongodb");
+// const url ='mongodb://localhost:27017/student-api';
+// const client = new MongoClient(url,{useNewUrlparser:true});
+// async function connectToDatabase(){
+//     try{
+//         await client.connect();
+//         console.log("connect to mongodb");
+//     }catch(err){
+//         console.error("Error connectinting to mongodb",err);
+//     }
+// }
+// connectToDatabase();
+// module.exports=student;
