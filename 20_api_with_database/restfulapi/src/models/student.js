a7938c47aee2918eb032fbbaa10e51e22ecc4fa8
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-11-06 22:19:30 +0530

    a new api added

--- 20_api_with_database/restfulapi/src/models/student.js
+++ 20_api_with_database/restfulapi/src/models/student.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const {MongoClient, ObjectId} = require("mongodb");
 const url = "mongodb://localhost:27017/student-api";
 const client = new MongoClient(url, { useNewUrlparser: true });
 async function connectToDatabase() {
@@ -16,37 +16,35 @@ const app = express();
 app.use(express.json());
 
 // app.express('express.json()')
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
   console.log("Hello from the home side");
-  res.send("Hello from the home page")
+  res.send("Hello from the home page");
 });
 
 // get method
 
-app.get("/create-student",async function(req,res){
+app.get("/create-student", async function (req, res) {
   try {
-  const database = client.db("student-api");
-  const collection = database.collection("add-Data");
-  const data = await collection.find({}).toArray();
+    const database = client.db("student-api");
+    const collection = database.collection("add-Data");
+    const data = await collection.find({}).toArray();
 
-  res.json(data);
-  console.log("show your data",data);
-  // res.send("show your data!");
-} catch (err) {
-  console.error("Your data is not created", err);
-  res.send("Your data is not created");
-}
-
-})
+    res.json(data);
+    console.log("show your data");
+    // res.send("show your data!");
+  } catch (err) {
+    console.error("Your data is not created", err);
+    res.send("Your data is not created");
+  }
+});
 
 // Post method
 
 app.post("/student", async (req, res) => {
   const database = client.db("student-api");
   const collection = database.collection("add-Data");
-  
 
   try {
     const data = {
@@ -63,4 +61,79 @@ app.post("/student", async (req, res) => {
     console.log("Error inserting data:", err);
   }
 });
+
+// Delete Method
+
+app.delete("/delete-data/:id", async function(req,res) {
+  console.log('inside the delete route')
+  const database = client.db("student-api");
+  const collection = database.collection("add-Data");
+  const idDelete = req.params.id;
+  console.log("user id", idDelete);
+  try {
+    const filter = { _id: new ObjectId(idDelete) };
+    console.log("filter in delete route", filter);
+    const result = await collection.deleteOne(filter);
+    if (result.deletedCount === 1) {
+      console.log("Data deleted successfully!");
+      res.send(`Data with ID ${idDelete} deleted successfully!`);
+    } else {
+      console.log(`No data found with ID ${idDelete}`);
+      res.status(404).send(`No data found with ID ${idDelete}`);
+    }
+  } catch (err) {
+    console.error("Error deleting data:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Update Method
+
+app.put("/user-update/:id", async function (req, res) {
+  const database = client.db("student-api");
+    const collection = database.collection("add-Data");
+
+  const idToUpdate = req.params.id;
+  console.log("update request id is ", idToUpdate);
+  
+  const filter = { _id: new ObjectId(idToUpdate) };
+  console.log("filter is ", filter);
+  try {
+    
+    const update = { $set: { name: "Ravi kumar", age: 22 } };
+    const result = await collection.updateOne(filter, update);
+    if (result.modifiedCount === 1) {
+      res.status(200).send("User updated successfully");
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    console.error("Error updating data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// //Get by id
+
+app.get("/users/:id", async function (req, res) {
+  const idToFind = req.params.id;
+  console.log("getById request id is ", idToFind);
+
+  try {
+    const database = client.db("student-api");
+    const collection = database.collection("add-Data");
+
+    const user = await collection.findOne({ _id: new ObjectId(idToFind) });
+
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    console.error("Error fetching data:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 app.listen(5000);
---
+++ 21_real_api_with_connection/restApi/index.js
@@ -0,0 +1,122 @@
+const { MongoClient, ObjectId } = require("mongodb");
+const url = "mongodb://localhost:27017/user-api";
+const client = new MongoClient(url, { UseNewUrlparser: true });
+async function connectToDb() {
+  try {
+    await client.connect();
+    console.log("you are connected to mongodb");
+  } catch (err) {
+    console.error("You can not connect to mongodb", err);
+  }
+}
+connectToDb();
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// Get method
+
+app.get("/", async function (req, res) {
+  try {
+    const database = client.db("user-api");
+    const collection = database.collection("userData");
+    const data = await collection.find({}).toArray();
+    res.json(data);
+    console.log("your data is created");
+  } catch (err) {
+    console.log("can not get data", err);
+    res.send("your data is not created");
+  }
+});
+
+// Post method
+
+app.post("/addData", async function(req,res){
+    try{
+        const database = client.db("user-api");
+        const collection = database.collection("userData")
+        const data ={
+            name: req.query.name,
+            age:parseInt(req.query.age),
+            gender:req.query.gender,
+            salary:parseInt(req.query.salary)
+
+        }
+        const result = await collection.insertOne(data);
+        console.log("your data is inserted done !",result);
+        res.send("your data is inserted done !")
+
+    }catch(err){
+        console.error("your data is not inserting",err);
+    }
+});
+
+// Delete method
+
+app.delete("/deleteData/:id",async function(req,res){
+  
+    const database = client.db("user-api");
+    const collection = database.collection("userData");
+    const idToDelete = req.params.id;
+    console.log("your delete id is",idToDelete);
+    try{
+      const filterValue = {_id: new ObjectId(idToDelete)};
+      const result = await collection.deleteOne(filterValue);
+      if(result.deletedCount === 1){
+        console.log("your data is deleted id",idToDelete);
+        res.send(`your data is deleted id ${idToDelete}`)
+      }else{
+        console.log("No data");
+        res.send("No data");
+      }
+    }catch(err){
+      console.log("Your data is not deleting");
+      res.send("Your data is not deleting");
+    }
+});
+
+// Update method
+
+app.put("/updateData/:id",async function(req,res){
+  const database = client.db("user-api");
+  const collection = database.collection("userData");
+  const idToUpdate = req.params.id;
+  try{
+    const filterValue = {_id: new ObjectId(idToUpdate)};
+    const updateResult ={$set:{name:"Suraj chuhan",age:18,gender:"male",salary:12000}}
+    const result = await collection.updateOne(filterValue,updateResult);
+    if(result.modifiedCount === 1){
+      console.log("Your data is modified",idToUpdate);
+      res.send(`Your data is modified id is ${idToUpdate}`)
+    }else{
+      console.log("Your data is not found",idToUpdate);
+      res.send(`Your data is not found ${idToUpdate}`)
+    }
+
+  }catch(err){
+    console.error("Your data is not updating",err);
+    res.send("Your data is not updating");
+  }
+});
+
+// Get by id mrthod
+
+app.get("/get/:id", async function(req,res){
+  const idToGet = req.params.id;
+  try{
+    const database = client.db("user-api");
+    const collection = database.collection("userData");
+    const getUser = await collection.findOne({_id: new ObjectId(idToGet)})
+    if(getUser){
+      res.json(getUser);
+    }else{
+      res.send("Getuser not found");
+    }
+  }catch(err){
+    console.error("your data is not getting",err);
+    res.send("Your data is not getting");
+  }
+})
+
+app.listen(8000);
